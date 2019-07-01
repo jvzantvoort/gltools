e2e0b62552f74271326f3238bf816eb86dddcf8c
John van Zantvoort <john.van.zantvoort@proxy.nl>
2019-07-01 18:46:56 +0200

    updated exceptions

--- gltools/exceptions.py
+++ gltools/exceptions.py
@@ -1,4 +1,19 @@
+"""Exceptions for the gltools package"""
+
+__author__ = "John van Zantvoort"
+__email__ = "john.van.zantvoort@proxy.nl"
+__copyright__ = "John van Zantvoort"
 
 
 class GLToolsException(Exception):
-    pass
+    """
+
+    :param message: the error message
+    :returns: Formatted exception message
+    """
+    def __init__(self, message):
+        self.message = message
+        Exception.__init__(self)
+
+    def __str__(self):
+        return "GLTools Error %s" % self.message
