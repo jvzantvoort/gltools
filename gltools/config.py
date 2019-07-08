5280f9cba8ce71840dfdc7ecb4d667db4d47c94d
John van Zantvoort <john.van.zantvoort@proxy.nl>
2019-07-08 18:58:46 +0200

    moved PyGitLabConfig from config to GitLabConfig in localgitlab

--- gltools/config.py
+++ gltools/config.py
@@ -8,10 +8,6 @@ import os
 import logging
 import copy
 
-try:
-    import ConfigParser as configparser
-except ImportError:
-    import configparser
 
 from yaml import load, dump
 try:
@@ -20,6 +16,8 @@ except ImportError:
     from yaml import Loader, Dumper
 
 from .exceptions import GLToolsException
+from .localgitlab import GitLabConfig
+
 
 
 class GitLabToolsConfig(object):
@@ -70,9 +68,9 @@ class GitLabToolsConfig(object):
                           'exportdir': os.path.expanduser('~/exports'),
                           'tempdir': os.path.expanduser('~/tmp')}
 
-        self.glconfig = PyGitLabConfig()
+        glconfig = GitLabConfig()
 
-        self.servername = kwargs.get('servername', self.glconfig.default)
+        self.servername = kwargs.get('servername', glconfig.default)
         self.groupname = kwargs.get('groupname')
         self.configfile = kwargs.get('configfile',
                                      os.path.expanduser('~/.gltools.cfg'))
@@ -135,164 +133,3 @@ class GitLabToolsConfig(object):
         return self.config.get('mask')
 
 
-class PyGitLabConfig(object):
-    """brief explanation
-
-    extended explanation
-
-    :param arg1: description
-    :param arg2: description
-    :type arg1: type description
-    :type arg1: type description
-    :return: return description
-    :rtype: the return type description
-
-    Example::
-
-      from gltools.config import PyGitLabConfig
-
-      config = PyGitLabConfig()
-
-    """
-    def __init__(self, **kwargs):
-
-        self.vars = dict()
-        self._args = dict()
-
-        self.globalopts = {'default': 'local',
-                           'ssl_verify': 'false',
-                           'timeout': 15}
-        self.sectionopts = {'url': 'http://localhost',
-                            'private_token': 'FIXME',
-                            'api_version': 4}
-
-        self.configfile = kwargs.get('configfile',
-                                     os.path.expanduser('~/.python-gitlab.cfg'))
-        self.logger = kwargs.get('logger', logging.getLogger('gltools'))
-
-        self.config = configparser.RawConfigParser()
-
-        for varname, varval in kwargs.items():
-            self._args[varname] = varval
-        self.loadconfig()
-
-    @property
-    def default(self):
-        """return the default section
-
-        :returns: default section
-        :rtype: str
-        """
-        return self.config.get('global', 'default')
-
-    def initconfig(self):
-        """initialize an empty conifguration
-
-        """
-
-        if not self.config.has_section('global'):
-            self.logger.debug("adding required section 'global'")
-            self.config.add_section('global')
-
-        for varname, varval in self.globalopts.items():
-            if varname in self._args:
-                self.logger.debug(
-                    "adding %s (value: %s) to global from arguments" %
-                    (varname, self._args[varname]))
-                self.config.set('global', varname, self._args[varname])
-            else:
-                self.logger.debug(
-                    "adding %s (value: %s) to global from defaults" %
-                    (varname, varval))
-                self.config.set('global', varname, varval)
-
-        if not self.config.has_section(self.default):
-            self.logger.debug("adding section '%s'" % self.default)
-            self.config.add_section(self.default)
-
-        for varname, varval in self.sectionopts.items():
-            if varname in self._args:
-                self.logger.debug(
-                    "adding %s (value: %s) to %s from arguments" %
-                    (varname, self._args[varname], self.default))
-                self.config.set(self.default, varname, self._args[varname])
-            else:
-                self.logger.debug(
-                    "adding %s (value: %s) to %s from section opts" %
-                    (varname, varval, self.default))
-                self.config.set(self.default, varname, varval)
-
-    def dump(self):
-        """Write the ini file content."""
-        self.config.write(sys.stdout)
-
-    def sect_global(self, name, varval):
-        """Set a global variable
-
-        :param name: the first value
-        :param varval: the first value
-        :type name: int, float,...
-        :type varval: int, float,...
-
-        Example::
-
-          config.sect_global("ssl_verify", "false")
-        """
-        self.set('global', name, varval)
-
-    def set(self, *args):
-        """short description
-
-        extended description
-
-        :param section: Configuration section
-        :param name: Configuration parameter name
-        :param value: Configuration parameter value
-        :type arg1: str
-        :type arg2: str
-        :type arg3: str
-        """
-
-        if len(args) == 3:
-            section = args[0]
-            name = args[1]
-            varval = args[2]
-
-        elif len(args) == 2:
-            section = self.default
-            name = args[0]
-            varval = args[1]
-
-        tmpdict = self.vars[section, {}]
-        tmpdict[name] = varval
-        self.vars[section] = tmpdict
-
-    def loadconfig(self):
-        """load the content from the configfile. If none exists a dummy will be
-        created.
-
-        :raises: GLToolsException if either the existsing configfile cannot be
-                 read or the configfile contains dummy data.
-        """
-        cfgfile = self.configfile
-
-        if not os.path.exists(cfgfile):
-            self.initconfig()
-            with open(cfgfile, 'w') as cfgh:
-                self.config.write(cfgh)
-        else:
-            if not self.config.read(cfgfile):
-                raise GLToolsException("failed to read configfile %s\n" % cfgfile)
-
-        private_token = self.config.get(self.default, 'private_token')
-        if private_token == 'FIXME':
-            raise GLToolsException(
-                "provided configfile %s is not valid, perhaps new?\n" % cfgfile)
-
-    @property
-    def configs(self):
-        """return available configs
-        :returns: list of configurations
-        :rtype: list of str
-        """
-        return [x for x in self.config.sections() if x != 'global']
--- gltools/localgitlab.py
+++ gltools/localgitlab.py
@@ -1,7 +1,11 @@
+"""This module handles the gitlab interactions."""
 
+
+from __future__ import print_function
+import os
 import logging
 
-from .exceptions import GLToolsException
+from gltools.exceptions import GLToolsException
 
 try:
     import gitlab
@@ -9,6 +13,165 @@ try:
 except ImportError:
     raise GLToolsException('python-gitlab module not available')
 
+try:
+    import ConfigParser as configparser
+except ImportError:
+    import configparser
+
+__author__ = "John van Zantvoort"
+__email__ = "john.van.zantvoort@proxy.nl"
+__copyright__ = "John van Zantvoort"
+__license__ = "proprietary"
+__version__ = "1.0.1"
+
+
+class GitLabConfig(object):
+    """Handle the gitlab config file
+
+    If it doesn't exist a dummy will be created.
+
+    :param arg1: description
+    :param arg2: description
+
+    :type arg1: type description
+    :type arg1: type description
+    :return: return description
+    :rtype: the return type description
+
+    Example::
+
+      from gltools.config import GitLabConfig
+
+      config = GitLabConfig()
+
+    """
+    def __init__(self, **kwargs):
+
+        self.vars = dict()
+        self._args = dict()
+
+        self.globalopts = {'default': 'local',
+                           'ssl_verify': 'false',
+                           'timeout': 15}
+        self.sectionopts = {'url': 'http://localhost',
+                            'private_token': 'FIXME',
+                            'api_version': 4}
+
+        self.configfile = kwargs.get('configfile',
+                                     os.path.expanduser('~/.python-gitlab.cfg'))
+        self.logger = kwargs.get('logger', logging.getLogger('gltools'))
+
+        self.config = configparser.RawConfigParser()
+
+        for varname, varval in kwargs.items():
+            self._args[varname] = varval
+
+        self.loadconfig()
+
+    @property
+    def default(self):
+        """return the default section
+
+        :returns: default section
+        :rtype: str
+        """
+        return self.config.get('global', 'default')
+
+    def set(self, *args):
+        """short description
+
+        extended description
+
+        :param section: Configuration section
+        :param name: Configuration parameter name
+        :param value: Configuration parameter value
+        :type arg1: str
+        :type arg2: str
+        :type arg3: str
+        """
+
+        if len(args) == 3:
+            section = args[0]
+            name = args[1]
+            varval = args[2]
+
+        elif len(args) == 2:
+            section = self.default
+            name = args[0]
+            varval = args[1]
+
+        tmpdict = self.vars[section, {}]
+        tmpdict[name] = varval
+        self.vars[section] = tmpdict
+
+    def initconfig(self):
+        """initialize an empty conifguration
+
+        """
+
+        if not self.config.has_section('global'):
+            self.logger.debug("adding required section 'global'")
+            self.config.add_section('global')
+
+        for varname, varval in self.globalopts.items():
+            if varname in self._args:
+                self.logger.debug(
+                    "adding %s (value: %s) to global from arguments" %
+                    (varname, self._args[varname]))
+                self.config.set('global', varname, self._args[varname])
+            else:
+                self.logger.debug(
+                    "adding %s (value: %s) to global from defaults" %
+                    (varname, varval))
+                self.config.set('global', varname, varval)
+
+        if not self.config.has_section(self.default):
+            self.logger.debug("adding section '%s'" % self.default)
+            self.config.add_section(self.default)
+
+        for varname, varval in self.sectionopts.items():
+            if varname in self._args:
+                self.logger.debug(
+                    "adding %s (value: %s) to %s from arguments" %
+                    (varname, self._args[varname], self.default))
+                self.config.set(self.default, varname, self._args[varname])
+            else:
+                self.logger.debug(
+                    "adding %s (value: %s) to %s from section opts" %
+                    (varname, varval, self.default))
+                self.config.set(self.default, varname, varval)
+
+
+    def loadconfig(self):
+        """load the content from the configfile. If none exists a dummy will be
+        created.
+
+        :raises: GLToolsException if either the existsing configfile cannot be
+                 read or the configfile contains dummy data.
+        """
+        cfgfile = self.configfile
+
+        if not os.path.exists(cfgfile):
+            self.initconfig()
+            with open(cfgfile, 'w') as cfgh:
+                self.config.write(cfgh)
+        else:
+            if not self.config.read(cfgfile):
+                raise GLToolsException("failed to read configfile %s\n" % cfgfile)
+
+        private_token = self.config.get(self.default, 'private_token')
+        if private_token == 'FIXME':
+            raise GLToolsException(
+                "provided configfile %s is not valid, perhaps new?\n" % cfgfile)
+
+
+    @property
+    def configs(self):
+        """return available configs
+        :returns: list of configurations
+        :rtype: list of str
+        """
+        return [x for x in self.config.sections() if x != 'global']
 
 class LocalGitLab(object):
     """Wrapper for the ``gitlab`` library
@@ -28,23 +191,73 @@ class LocalGitLab(object):
 
     def __init__(self, **kwargs):
 
-        self.server = "local"
-        self.groupname = "default"
+        self._server = "local"
+        self._groupname = "default"
+        self._groups = list()
+        self._gitlab = None
+        self.config = None
 
         props = ('server', 'groupname')
         for prop in props:
+            pname = "_" + prop
             if prop in kwargs:
-                setattr(self, prop, kwargs[prop])
-        self._gitlab = gitlab.Gitlab.from_config(self.server)
-        self._groups = list()
+                setattr(self, pname, kwargs[prop])
+
+        self.loadconfig()
 
         self.logger = kwargs.get('logger', logging.getLogger('gltools'))
+
+
+    def check_servername(self, sectionname):
+        """Check if the section name is valid
+
+        :param sectionname: name of the section in the ``~/.python-gitlab.cfg`` file
+        :type arg1: str
+        :returns: True if valid
+        :rtype: bool
+        """
+        if sectionname in self.config.configs:
+            return True
+        return False
+
+
+    def loadconfig(self):
+        """(re)load the configfile"""
+        self.config = GitLabConfig(servername=self.server,
+                                   groupname=self.groupname)
+
+    @property
+    def groupname(self):
+        """The name of the gitlab group used."""
+        return self._groupname
+
+    @groupname.setter
+    def groupname(self, varval):
+        self._groupname = varval
+        self.loadconfig()
+        return self._groupname
+
+    @property
+    def server(self):
+        """The name of the gitlab server definition used."""
+        return self._server
+
+    @server.setter
+    def server(self, varval):
+        if not self.check_servername(varval):
+            raise GLToolsException("%s is an invalid configname" % varval)
+        self._server = varval
+        self._gitlab = gitlab.Gitlab.from_config(self._server)
+        self.loadconfig()
 
     @property
     def groups(self):
         """return the gitlab group objects visible to the user"""
         if self._groups:
             return self._groups
+
+        if self._gitlab is None:
+            self._gitlab = gitlab.Gitlab.from_config(self.server)
 
         for obj in self._gitlab.groups.list(all=True):
             self._groups.append(obj)
@@ -139,3 +352,7 @@ class LocalGitLab(object):
         for project in obj.projects.list(all=True):
             retv.append(self.getproj(project))
         return retv
+
+if __name__ == '__main__':
+    GL = LocalGitLab()
+    print(GL.grouptree('homenet'))
