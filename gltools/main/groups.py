7e13c03195b6200d95ac5123443fa935780230fc
John van Zantvoort <john.van.zantvoort@proxy.nl>
2019-07-11 01:30:57 +0200

    added group listing

---
+++ gltools/main/groups.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""gltools.main.groups """
+
+import sys
+import os
+import logging
+from gltools.main.common import Main
+from gltools.localgitlab import QueryGitLab
+from gltools.exceptions import GLToolsException
+
+log = logging.getLogger('gltools.main.groups')
+
+__author__ = "John van Zantvoort"
+__copyright__ = "Proxy B.V."
+__email__ = "john.van.zantvoort@proxy.nl"
+
+class ListGroups(Main):
+    """List the groups on the gitlab server
+
+    :param gitlab_config_section: section in the python-gitlab configuration
+                                  file.
+    :param logger: logging object if any
+    :type arg1: str
+    :type arg1: class
+    """
+
+    def __init__(self, **kwargs):
+        super(ListGroups, self).__init__(**kwargs)
+
+    def main(self):
+        for groupname in self.gitlab.groupnames:
+            print(groupname)
