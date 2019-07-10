8a286646b6ce8dab04533afe4b27309755cc94d9
John van Zantvoort <john.van.zantvoort@proxy.nl>
2019-07-11 01:31:50 +0200

    added ListGroups, ListProjects and InitConfig to gltools.main

--- gltools/main/__init__.py
+++ gltools/main/__init__.py
@@ -1,3 +1,6 @@
 from gltools.main.exportgroup import ExportGroup
 from gltools.main.workongroup import WorkOnGroup
 from gltools.main.syncgroup import SyncGroup
+from gltools.main.groups import ListGroups
+from gltools.main.projects import ListProjects
+from gltools.main.config import InitConfig
